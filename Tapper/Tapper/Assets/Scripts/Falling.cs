3178c9b8cb55cbdea2b898162c638e5a07e40086
Jack Gola <thejackgola@gmail.com>
2017-05-05 00:46:52 -0400

    First Commit
    Main function now works, the falling circle now destroys when it
    collides with the "player" circle.

---
+++ Tapper/Tapper/Assets/Scripts/Falling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falling : MonoBehaviour {
+
+	public float fallSpeed = 4.0f;
+	//public float spinSpeed = 250.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+		//transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+	
+	}
+}
